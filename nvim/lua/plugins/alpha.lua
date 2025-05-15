fc3c4c4c8b4d27c0f9639bbc8e737e89363cd9c6
dean <dfw662@gmail.com>
2025-05-15 16:43:23 +0100

    Remove New file option in alpha

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,6 +1,6 @@
 return {
 	'goolord/alpha-nvim',
-	config = function ()
+	config = function()
 		local alpha = require('alpha')
 		local dashboard = require('alpha.themes.dashboard')
 
@@ -34,11 +34,10 @@ return {
 		dashboard.section.header.val = emoji
 
 		dashboard.section.buttons.val = {
-			dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-			dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
-			dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-			dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-			dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+			dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
+			dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+			dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 		}
 
 		alpha.setup(dashboard.config)
