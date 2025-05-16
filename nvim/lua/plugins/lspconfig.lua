d401e23dc21fed141333039a3310f7c80d6d955b
dean <dfw662@gmail.com>
2025-05-16 10:13:24 +0100

    Resolve errors in Mason update

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,68 +1,56 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
+	event = { "BufReadPre", "BufNewFile"},
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true }
 	},
 	config = function()
-		local mason = require("mason-lspconfig")
-		local lspconfig = require("lspconfig")
 		local cmp = require("cmp_nvim_lsp")
 		local caps = cmp.default_capabilities()
-
-		mason.setup_handlers({
-			function(server)
-				lspconfig[server].setup({ capabilities = caps })
-			end,
-			["lua_ls"] = function()
-				lspconfig["lua_ls"].setup({
-					capabilities = caps,
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
+		vim.lsp.config("lua_ls", {
+			capabilities = caps,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = {"vim"},
 					},
-				})
-			end,
-			["omnisharp"] = function()
-				local homeDir = os.getenv("HOME")
-				lspconfig["omnisharp"].setup({
-					capabilities = caps,
-					on_attach = on_attach,
-					cmd = { "dotnet", homeDir .. "/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
-					-- Enables support for reading code style, naming convention and analyzer
-					-- settings from .editorconfig.
-					enable_editorconfig_support = true,
-					-- If true, MSBuild project system will only load projects for files that
-					-- were opened in the editor. This setting is useful for big C# codebases
-					-- and allows for faster initialization of code navigation features only
-					-- for projects that are relevant to code that is being edited. With this
-					-- setting enabled OmniSharp may load fewer projects and may thus display
-					-- incomplete reference lists for symbols.
-					enable_ms_build_load_projects_on_demand = false,
-					-- Enables support for roslyn analyzers, code fixes and rulesets.
-					enable_roslyn_analyzers = false,
-					-- Specifies whether 'using' directives should be grouped and sorted during
-					-- document formatting.
-					organize_imports_on_format = true,
-					-- Enables support for showing unimported types and unimported extension
-					-- methods in completion lists. When committed, the appropriate using
-					-- directive will be added at the top of the current file. This option can
-					-- have a negative impact on initial completion responsiveness,
-					-- particularly for the first few completion sessions after opening a
-					-- solution.
-					enable_import_completion = true,
-					-- Specifies whether to include preview versions of the .NET SDK when
-					-- determining which version to use for project loading.
-					sdk_include_prereleases = true,
-					-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-					-- true
-					analyze_open_documents_only = false,
-				})
-			end,
+				},
+			},
+		})
+		vim.lsp.config("lua_ls", {
+
+			capabilities = caps,
+			on_attach = on_attach,
+			cmd = { "dotnet", "/home/dfw-work/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+			-- Enables support for reading code style, naming convention and analyzer
+			-- settings from .editorconfig.
+			enable_editorconfig_support = true,
+			-- If true, MSBuild project system will only load projects for files that
+			-- were opened in the editor. This setting is useful for big C# codebases
+			-- and allows for faster initialization of code navigation features only
+			-- for projects that are relevant to code that is being edited. With this
+			-- setting enabled OmniSharp may load fewer projects and may thus display
+			-- incomplete reference lists for symbols.
+			enable_ms_build_load_projects_on_demand = false,
+			-- Enables support for roslyn analyzers, code fixes and rulesets.
+			enable_roslyn_analyzers = false,
+			-- Specifies whether 'using' directives should be grouped and sorted during
+			-- document formatting.
+			organize_imports_on_format = true,
+			-- Enables support for showing unimported types and unimported extension
+			-- methods in completion lists. When committed, the appropriate using
+			-- directive will be added at the top of the current file. This option can
+			-- have a negative impact on initial completion responsiveness,
+			-- particularly for the first few completion sessions after opening a
+			-- solution.
+			enable_import_completion = true,
+			-- Specifies whether to include preview versions of the .NET SDK when
+			-- determining which version to use for project loading.
+			sdk_include_prereleases = true,
+			-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+			-- true
+			analyze_open_documents_only = false,
 		})
 	end
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -13,11 +13,21 @@ return {
 
 			lspconfig.setup({
 				ensure_installed = {
-					"lua_ls",
+					"lua_ls" ,
 					"terraformls",
 					"omnisharp",
 				},
 			})
-		end
-	}
+		end,
+		opts = {
+			handlers = {
+				function (name)
+					local lspconfig = require('lspconfig')
+					local cmp = require("cmp_nvim_lsp")
+					local caps = cmp.default_capabilities()
+					lspconfig[name].setup({capabilities = caps})
+				end,
+			},
+		},
+	},
 }
